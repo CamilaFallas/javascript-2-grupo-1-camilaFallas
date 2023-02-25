1a11d1d02d223d1d70050b2aa522162582607954
CamilaFallas <camilafallasnaranjo@gmail.com>
2023-02-25 08:08:23 -0600

    add exercise

--- main.js
+++ main.js
@@ -1,15 +1,140 @@
-// 1. Escriba una function que escriba un número a la inversa
+// const product = {
+//   name: 'productName',
+//   price: 200,
+//   quantity: 50,
+//   tax: 13,
+//   category: ['home'],
+//   subCategory: 'furniture',
+//   total: function() {
+//       return price + (price * this.tax / 100);
+//   },
+// }
 
-const numero = 123456;
-function inversa (n) {
-  const stringUnir = n.toString().split("").reverse().join("");
-  return Number(stringUnir); 
+//dot notation
+// console.log(product.category);
+// product.nuevoKey = 'es un nuevo key';
+
+// product.category.push('xcategory');
+
+// delete product.subCategory
+
+//bracket notation
+// console.log(product['category']);
+
+// console.log(product);
+
+
+// =======================================================================================
+// ============================= EJEMPLOS OOP ============================================
+
+// const estudianteUno = {
+//   nombre: 'Andy',
+//   apellidos: 'Smith',
+//   fechaNacimiento: '1990-10-30',
+//   id: 'a-0001',
+//   nuevoIngreso: true,
+//   carrera: 'Historía del Arte',
+//   cursosMatriculados: ['Introducción a la pintura', 'Historia del Arte I'],
+//   cursosCursados: [],
+// }
+
+// function estudianteNuevo(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, cursosMatriculados, cursosCursados) {
+//   const datos = {};
+
+//   datos.nombre = nombre;
+//   datos.apellidos = apellidos;
+//   datos.fechaNacimiento = fechaNacimiento;
+//   datos.id = id;
+//   datos.nuevoIngreso = nuevoIngreso;
+//   datos.carrera = carrera;
+//   datos.cursosMatriculados = cursosMatriculados || [];
+//   datos.cursosCursados = cursosCursados || [];
+  
+//   datos.matricular = function(curso) {
+//     this.cursosMatriculados.push(curso)
+//   }
+
+//   datos.retirarMatricula = function(curso) {
+//     const index = this.cursosMatriculados.indexOf(curso);
+//     if (index > -1) {
+//       this.cursosMatriculados.splice(index, 1);
+//     }
+
+//     // this.cursosMatriculados = this.cursosMatriculados.filter(item => item !== curso); // devuelve un array con los elementos que cumplen con la condición
+//   }
+
+//   return datos;
+// }
+
+// const andy = estudianteNuevo('Andy', 'Smith', '1990-10-30', 'a-0001', true, 'Historía del Arte', ['Introducción a la pintura']);
+// const ana =  estudianteNuevo('Ana', 'Patel', '1990-10-30', 'arq-0001', true, 'Arquitectura', ['Introducción a la pintura', 'Historia del Arte I']);
+// console.log(ana);
+
+// const listaEstudiantes = [];
+// listaEstudiantes.push(andy)
+// listaEstudiantes.push(ana)
+
+// console.log(listaEstudiantes);
+// console.log(andy);
+
+// andy.matricular('Historia del Arte I');
+// ana.retirarMatricula('Historia del Arte I')
+
+//console.log(andy)
+
+// console.log(listaEstudiantes);
+
+
+// **** Constructors ****
+function EstudianteConstructor(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, cursosMatriculados, cursosCursados) {
+  this.nombre = nombre;
+  this.apellidos = apellidos;
+  this.fechaNacimiento = fechaNacimiento;
+  this.id = id;
+  this.nuevoIngreso = nuevoIngreso;
+  this.carrera = carrera;
+  this.cursosMatriculados = cursosMatriculados || [];
+  this.cursosCursados = cursosCursados || [];
+
+  this.matricular = function(curso) {
+    this.cursosMatriculados.push(curso)
+  }
+
+  this.retirarMatricula = function(curso) {
+    const index = this.cursosMatriculados.indexOf(curso);
+    if (index > -1) {
+      this.cursosMatriculados.splice(index, 1);
+    }
+
+    // this.cursosMatriculados = this.cursosMatriculados.filter(item => item !== curso); // devuelve un array con los elementos que cumplen con la condición
+  }
 }
-console.log(inversa(numero)); 
 
-// 2. Escriba una function que acepte un string como parámetro y convierta cada primer letra de cada palabra en mayúscula
+// const estudiantePrueba = new EstudianteConstructor('Test', 'Testing', '1990-10-30', 'arq-0001', false, 'Arquitectura', ['Introducción a la pintura', 'Historia del Arte I'], [{curso: 'Prueba', nota: 50 }])
 
-function mayuscula (frase) {
-  return frase[0].toUpperCase() + frase.slice(1);
+//console.log(estudiantePrueba);
+
+
+// 1. Crear un método que permita agregar cursos a la lista de cursos aprobados. 
+//El usuario debe poder ingresar un objeto con el nombre del curso, nota.
+// 2. Y dependiendo la nota se agrega de manera automática una propiedad 
+//nueva que se llama estado: cuyo valor sera de aprobado (nota > 70) o reprobado (nota < 70)
+
+this.notaObtenida = function(curso, nota){
+  const calificaciones = {};
+
+  calificaciones.curso = curso;
+  calificaciones.nota = nota;
+
+  if(nota >= 70){
+    calificaciones.estado = "aprobado"
+  } else {
+    calificaciones.estado = "reprobado"
+  }
+
+  this.cursosCursados.push(calificaciones);
 }
-console.log(mayuscula("hola como estas"));
+
+const andy = new EstudianteConstructor('Andy', 'Smith', '1990-10-30', 'arq-0001', true, 'Historía del Arte', ['Historia del Arte I']);
+andy.notaObtenida('Introducción a la pintura', 90);
+console.log(andy);
