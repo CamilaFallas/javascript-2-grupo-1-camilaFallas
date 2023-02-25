a0ca3b55a5cf84b77992e40cf439b9c84843bc8e
CamilaFallas <camilafallasnaranjo@gmail.com>
2023-02-25 08:29:03 -0600

    set and get exercise

--- main.js
+++ main.js
@@ -1,117 +1,135 @@
-// Prueba Diagnostico
 
-// I Parte: Teoría
+// // **** Constructors ****
+// function EstudianteConstructor(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, cursosMatriculados, cursosCursados) {
+//   this.nombre = nombre;
+//   this.nombre = nombre;
+//   this.apellidos = apellidos;
+//   this.fechaNacimiento = fechaNacimiento;
+//   this.id = id;
+//   this.nuevoIngreso = nuevoIngreso;
+//   this.carrera = carrera;
+//   this.cursosMatriculados = cursosMatriculados || [];
+//   this.cursosCursados = cursosCursados || [];
 
-// 1. Respuesta pregunta 1
+//   this.matricular = function(curso) {
+//     this.cursosMatriculados.push(curso)
+//   }
 
-// Numero: Se escribe sin comillas, representa un valor y estos pueden ser enteros Ej 3 o decimales Ej 3.3
-//String: Son letras, se escriben dentro de comillas, sino serian valores predeterminados de js, caracteres en general de texto. Ej:'Hola'
-//Buleano: Tiene valor de verdadero o falso. true false
-//Undefine: No conocemos el valor de la variable. 
+//   this.retirarMatricula = function(curso) {
+//     const index = this.cursosMatriculados.indexOf(curso);
+//     if (index > -1) {
+//       this.cursosMatriculados.splice(index, 1);
+//     }
 
-// ====================================================================================================
+//     // this.cursosMatriculados = this.cursosMatriculados.filter(item => item !== curso); // devuelve un array con los elementos que cumplen con la condición
+//   }
 
+//   this.agregarCursosCursado = function (curso, nota) {
+//     const cursoObj = {curso, nota};
+//     cursoObj.estado = nota >= 70 ? 'aprobado' : 'reprobado';
 
-// 2. Respuesta pregunta 2
+//     if(nota >= 70) {
+//       cursoObj.estado = 'aprobado'
+//     } else {
+//       cursoObj.estado = 'reprobado'
+//     }
 
-// let personas = { nombre: "Sofia", nombre: "Pedro", nombre: "Carlos" };
-//personas.nombre = 'Ericka';
+//     this.cursosCursados.push(cursoObj)
+//   }
+// }
 
-// let personas = { nombre: "Sofia", nombre: "Pedro", nombre: "Carlos" };
-// personas['nombre'] = "Ericka";
+// const estudiantePrueba = new EstudianteConstructor('Test', 'Testing', '1990-10-30', 'arq-0001', false, 'Arquitectura', ['Introducción a la pintura', 'Historia del Arte I'], [{curso: 'Prueba', nota: 50 }])
 
-// ====================================================================================================
-
-
-// 3. Respuesta pregunta 3
-
-// +=: Aca el console.log de list me va a mostrar la suma del valor de la variable declarada arriba con el valor del numero agregado con += 
-// el segundo console.log me va a mostrar el resultado nuevamente de la suma anterior junto con el texto agregado de 'textoDePrueba', todo esto como un string.
-
-// ====================================================================================================
-
-
-// 4. Respuesta pregunta 4
-
-//!= y !== me compara la negacion por decirlo de una forma, a diferencia de que != no es estricto, o sea no le importan si compara 10 con '10', el 
-// lo hace, en cambio !== si toma en cuenta como este ingresado el dato.
-
-// != : No es estricto asi que daria false
-//!== : Daria False
-
-// != : Daria False ya que compara numero ignorando que este entre comillas como un string
-// !== :Daria  True ya que compara el number con string
-
-
-// ====================================================================================================
+// // console.log(estudiantePrueba);
 
 
-// 5. Respuesta pregunta 5
+// //Prototypes
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//   this.info = function() {
+//       return `Name: ${this.name}. Age: ${this.age}`;
+//   };
+//   // this.gender = 'male';
+// }
 
-// Funciones que se encuentran almacenadas dentro de una variable y solo se pueden utilizar llamando esa variable
+// Person.prototype.gender = "male";
 
-// ====================================================================================================
+// const person1 = new Person('Alex', '30');
+// const person2 = new Person('John', '25');
+
+// // prototype value of Person
+// // console.log(Person.prototype);
+
+// // inheriting the property from prototype
+// // console.log(person1.gender); // output: male
+
+// // por qué agregar propiedades a la funciónn constructor usando el prototype y no directamente en el constructor?
+// // porque si se agregan directamente en el constructor, se van a crear tantas copias de la propiedad como objetos se creen a partir de ese constructor, y eso es ineficiente
+// // por eso se agregan al prototype, porque así se crea una sola copia de la propiedad, y todos los objetos creados a partir de ese constructor van a heredar esa propiedad
+
+// //cómo modificar una propiedad del prototype? con la palabra reservada prototype seguida del nombre del constructor, y luego el nombre de la propiedad que se quiere modificar
+// // ejemplo de modificación de una propiedad del prototype de un constructor
+
+// Person.prototype.gender = 'unknown';
+
+// // console.log(Person.prototype);
+// // console.log(person1.gender);
+// // console.log(person2.gender);
+
+// Object.prototype.hello = function() {
+//   return 'Hola Mundo!';
+// };
+
+// const miObjeto = {};
+// // console.log(miObjeto.hello());
 
 
-// 6. Respuesta pregunta 6
+// // Classes
+// class Persona {
+//   constructor(nombre, publicaciones) {
+//     this.nombre = nombre;
+//     this.publicaciones = publicaciones || [];
+//   }
 
-// Una funcion declarada es aquella que hago en termino generales, fuera de funciones por ejemplo que me va a aplicar con el mismo valor y
-//no la puedo volver a declarar mas adelante dentro del js, en cambio las de expresion estan dentro de las funciones y tiene ese valor para
-//ese espacio en especifico y se puede declarar mas adelante dentro de otra funcion.
+//   // setter
+//   set setPublicacion(publicacion) {
+//     this.publicaciones.push(publicacion);
+//   }
 
-// ====================================================================================================
+//   // getter 
+//   get getPublicaciones() {
+//     return this.publicaciones;
+//   }
+// }
 
+// const persona1 = new Persona('Pedro');
+// laura.setPublicacion = { titulo: 'Cómo hacer un postre', contenido: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.'};
+//console.log(persona1.getPublicaciones);
 
-// 7. Respuesta pregunta 7
-
-// Funciones o sea porciones de codigo que estan dentro de un objeto en especifico
-
-// ====================================================================================================
-
-
-// 8. Respuesta pregunta 8
-
-// La recursividad es cuando desgrano un problema de atras para adelante, la intencion es que la funcion se pueda llamar e ir desgranando 
-
-// ====================================================================================================
-
-
-// II Parte: Ejercicios
-
-// Crear un programa que permita separar una lista de números aleatorios en
-// dos listas nuevas, de manera que se separen los valores en pares e impares.
-// Dicho programa debe recibir una lista de números como parámetro e iterar
-// dicha lista, de manera que se pueda verificar si el número es par o impar y
-// agrupar correctamente en la lista de números pares o impares. Al final debe
-// imprimirse ambas listas.
-
-// 1. Respuesta pregunta 1
-
-let lista= [1,2,3,345,45,23,12,34,38,37,8,6,16];
-let pares = [];
-let impares = [];
-
-function parImpar (lista) {
-  for (let i=0; i<lista.length;i++) {
-      if (lista[i]%2==0)
-      {
-          pares.push(lista[i]);
-      }
-      else
-      {
-          impares.push(lista[i]);
-      }
+class Estudiante {
+  constructor(nombre, apellidos, fechaNacimiento, id, nuevoIngreso, carrera, cursosMatriculados, cursosCursados) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.fechaNacimiento = fechaNacimiento;
+    this.id = id;
+    this.nuevoIngreso = nuevoIngreso;
+    this.carrera = carrera;
+    this.cursosMatriculados = cursosMatriculados || [];
+    this.cursosCursados = cursosCursados || [];
   }
-console.log ('Lista de numeros pares es ' + pares)
-console.log ('Lista de numeros impares es ' + impares)
+
+  set setMatricular(curso) {
+    this.cursosMatriculados.push(curso);
   }
-parImpar (lista,pares,impares);
 
-// ====================================================================================================
+  get getMatriculas(){
+    return this.cursosMatriculados;
+  }
+}
 
+const andy = new Estudiante('Andy', 'Smith', '1990-10-30', 'arq-0001', false, 'Arquitectura');
+andy.setMatricular = 'Introducción a la pintura';
+console.log(andy.getMatriculas);
+console.log(andy.getRetirarMatriculas);
 
-// 2. Respuesta pregunta 2
-
-// Colocar el código acá
-
-// ====================================================================================================
